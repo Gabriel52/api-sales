98d121f96fbd40f5a0ea6f612bc3bb0b4c92ff23
Gabriel52 <gabrielbrune52@gmail.com>
2024-02-14 21:08:36 -0300

    chore: add reset password service

---
+++ src/modules/users/services/ResetPasswordService.ts
@@ -0,0 +1,42 @@
+import { isAfter, addHours } from 'date-fns';
+
+import { getCustomRepository } from 'typeorm';
+import AppError from '@shared/errors/AppError';
+import { UserRepository } from '../typeorm/repositories/UsersRepository';
+import { UserTokensRepository } from '../typeorm/repositories/UserTokensRepository';
+import { SALT, TIME_TO_TOKEN_VALIDATION_EMAIL } from '@shared/const';
+import { hash } from 'bcryptjs';
+
+interface IRequest {
+  token: string;
+  password: string;
+}
+
+class ResetPasswordService {
+  public async execute({ token, password }: IRequest): Promise<void> {
+    const userRepository = getCustomRepository(UserRepository);
+    const userTokenRepository = getCustomRepository(UserTokensRepository);
+
+    const userToken = await userTokenRepository.findByToken(token);
+
+    if (!userToken) {
+      throw new AppError('User token does not exists.');
+    }
+    const user = await userRepository.findById(userToken.id);
+
+    if (!user) {
+      throw new AppError('User does not exists.');
+    }
+
+    const tokenCreateAt = userToken.created_at;
+    const compareDAte = addHours(tokenCreateAt, TIME_TO_TOKEN_VALIDATION_EMAIL);
+
+    if (isAfter(Date.now(), compareDAte)) {
+      throw new AppError('Token expired');
+    }
+    user.password = await hash(password, SALT);
+    userRepository.save(user);
+  }
+}
+
+export { ResetPasswordService };
--- src/shared/const/index.ts
+++ src/shared/const/index.ts
@@ -3,3 +3,5 @@ export const SALT = 8; // TODO: move out this const to dotenv file
 export const SECRET_HASH = '7b61055c326e423561f44447966c2461'; // TODO: move out this const to dotenv file
 
 export const TIME_TOKEN_VALIDATION = '1d';
+
+export const TIME_TO_TOKEN_VALIDATION_EMAIL = 2; //2 hours
