9cb5aed41c410baf23ee7c7ba009cbb93b415001
Gabriel52 <gabrielbrune52@gmail.com>
2024-02-17 14:37:59 -0300

    chore: add router to forgot password

---
+++ src/modules/users/routes/password.routes.ts
@@ -0,0 +1,18 @@
+import { celebrate, Joi, Segments } from 'celebrate';
+import { Router } from 'express';
+import { ForgotPasswordController } from '../controllers/ForgotPasswordController';
+
+const passwordRouter = Router();
+const forgotPasswordController = new ForgotPasswordController();
+
+passwordRouter.post(
+  '/forgot',
+  celebrate({
+    [Segments.BODY]: {
+      email: Joi.string().email().required(),
+    },
+  }),
+  forgotPasswordController.create,
+);
+
+export { passwordRouter };
